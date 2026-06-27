1808e99d644c6f45357b39268c1e4f11459d6932
1122-liang <1122-liang@users.noreply.github.com>
2026-06-27 22:28:59 +0800

    deploy: 2026-06-27 22:28

---
+++ xq-watchlist-data.js
@@ -0,0 +1,91 @@
+window.__SNAPSHOT__ = {
+  "updated_at": "2026-06-27T18:16:35",
+  "count": 6,
+  "stocks": [
+    {
+      "symbol": "SZ300776",
+      "name": "帝尔激光",
+      "marketplace": "CN",
+      "watched": "2026-06-26T10:33:51"
+    },
+    {
+      "symbol": "SZ000725",
+      "name": "京东方A",
+      "marketplace": "CN",
+      "watched": "2026-06-26T10:12:16"
+    },
+    {
+      "symbol": "SZ001389",
+      "name": "广合科技",
+      "marketplace": "CN",
+      "watched": "2026-06-26T10:08:02"
+    },
+    {
+      "symbol": "SZ300604",
+      "name": "长川科技",
+      "marketplace": "CN",
+      "watched": "2026-06-26T10:01:42"
+    },
+    {
+      "symbol": "SH603773",
+      "name": "沃格光电",
+      "marketplace": "CN",
+      "watched": "2026-06-26T09:43:14"
+    },
+    {
+      "symbol": "SH600667",
+      "name": "太极实业",
+      "marketplace": "CN",
+      "watched": "2026-06-26T09:32:08"
+    }
+  ]
+};
+window.__CHANGELOG__ = [
+  {
+    "date": "2026-06-26",
+    "changes": [
+      {
+        "time": "10:33",
+        "action": "add",
+        "symbol": "SZ300776",
+        "name": "帝尔激光",
+        "watched": "2026-06-26T10:33:51"
+      },
+      {
+        "time": "10:12",
+        "action": "add",
+        "symbol": "SZ000725",
+        "name": "京东方A",
+        "watched": "2026-06-26T10:12:16"
+      },
+      {
+        "time": "10:08",
+        "action": "add",
+        "symbol": "SZ001389",
+        "name": "广合科技",
+        "watched": "2026-06-26T10:08:02"
+      },
+      {
+        "time": "10:01",
+        "action": "add",
+        "symbol": "SZ300604",
+        "name": "长川科技",
+        "watched": "2026-06-26T10:01:42"
+      },
+      {
+        "time": "09:43",
+        "action": "add",
+        "symbol": "SH603773",
+        "name": "沃格光电",
+        "watched": "2026-06-26T09:43:14"
+      },
+      {
+        "time": "09:32",
+        "action": "add",
+        "symbol": "SH600667",
+        "name": "太极实业",
+        "watched": "2026-06-26T09:32:08"
+      }
+    ]
+  }
+];
